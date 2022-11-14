4240553ee58e8b77c21ae08d9be39417d49724de
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2022-11-14 16:50:10 +0100

    it worky ig

--- client/stream.lua
+++ client/stream.lua
@@ -1,6 +1,6 @@
 local streamURL = "http://localhost:3000"
 local xSize, ySize = term.getSize()
-local mainWindow = window.create(term.current(), 1, 1, xSize, ySize, true)
+local mainWindow = window.create(term.current(), 1, 1, xSize, ySize - 1, true)
 local targetFPS = 30
 
 local args = {...}
@@ -13,30 +13,94 @@ else
     program = shell.resolve(args[1])
 end
 
+local mainTerm = term.current()
 
-local function convertHTML(buffer)
-  local html = ""
-  for i,o in pairs(buffer) do
-    --print(textutils.serialise(o))
-    local line = "<a>" .. o[1] .. "</a>\n"
-    html = html .. line
+local function convertHTML(tbl)
+  -- convert to chars per pixel
+  local charTable = {}
+  for i,line in pairs(tbl) do -- loop over lines
+    local buffer = {}
+    for charI=1, #line[1] do
+        table.insert(buffer,{line[1]:sub(charI,charI),line[2]:sub(charI,charI),line[3]:sub(charI,charI)})
+    end
+    table.insert(charTable,buffer)
   end
-  return {html}
+  tbl = charTable
+  -- convert to same color chunks
+  local fragLines = {}
+  for il, line in pairs(tbl) do
+    local fragChunks = {}
+    local currentChunk = {}
+    for ip, pixel in pairs(line) do 
+        if not currentChunk[#currentChunk] then
+        elseif pixel[2] ~= currentChunk[#currentChunk][2] then
+            table.insert(fragChunks,currentChunk)
+            currentChunk = {}
+        elseif pixel[3] ~= currentChunk[#currentChunk][3] then
+            table.insert(fragChunks, currentChunk)
+            currentChunk = {}
+        end
+        table.insert(currentChunk,{pixel[1],pixel[2],pixel[3]})
+        --print(currentChunk[#currentChunk][1])
+    end
+    table.insert(fragLines,fragChunks)
+    --print(textutils.serialise(fragChunks))
+  end
+  
+  -- group similar strings
+  local lines = {}
+  for il, line in pairs(fragLines) do
+    local currentLine = {}
+    for ic, chunk in pairs(line) do
+        local collectiveTXT = ""
+        for isc, subChunk in pairs(chunk) do
+            collectiveTXT = collectiveTXT .. subChunk[1]
+        end
+        table.insert(currentLine,{collectiveTXT,chunk[1][2],chunk[1][2]})
+    end
+    table.insert(lines,currentLine)
+  end
+
+  -- convert to html
+  local html = "<link href=\"" .. streamURL .. "/stream.css\" type=\"text/css\" rel=\"stylesheet\"/>  <meta http-equiv=\"refresh\" content=\"1\" />"
+  for i,line in pairs(lines) do -- loop over lines
+    local htmlChunk = "<div> "
+    for ic, chunk in pairs(line) do
+        htmlChunk = htmlChunk .. "<pre class=\"fg" .. chunk[2] .. "\" class=\"bg" .. chunk[3] .. "\"> " .. chunk[1] .. "</pre>"
+    end
+    if htmlChunk == "<div> " then
+        htmlChunk = htmlChunk .. "<p></p>"
+    end
+    htmlChunk = htmlChunk .. "</div>"
+    html = html .. htmlChunk 
+  end
+  return html
 end
 
+local realFPS = 0
+
+local function drawStatus()
+    local oldX, oldY = mainTerm.getCursorPos()
+    mainTerm.setCursorPos(1,ySize)
+    mainTerm.write(realFPS .. " / " .. targetFPS .. " " .. os.time())
+    mainTerm.setCursorPos(xSize - #"LIVE",ySize)
+    mainTerm.write("LIVE")
+    mainTerm.setCursorPos(oldX,oldY)
+end
 
 local function runProgram()
   term.redirect(mainWindow)
   shell.run(program)
+  mainTerm.clear()
+  mainTerm.setCursorPos(1,1)
 end
 
 local function pollDisplay()
-  while true do
     local startTime = os.epoch()
     local mainBuffer = {}
-    local getLine = mainWindow.getLine
+    local _,ySize = mainWindow.getSize()
     for i=1,ySize do -- thanks 9551dev for the optimization
-        local txt,fg,bg = getLine(i)
+        local txt,fg,bg = mainWindow.getLine(i)
         mainBuffer[i] = {
             txt,fg,bg
         }
@@ -49,15 +113,31 @@ local function pollDisplay()
         term.write("Frame took " .. endTime - startTime .. "ms, that is " .. math.floor(1/(endTime / 1000 - startTime / 1000)) .. " fps")
         mainWindow.setCursorPos(xPos, yPos)
     end
+    realFPS = math.floor(1/(endTime / 1000 - startTime / 1000))
+
     if not benchmarkMode then
-      http.post(streamURL,textutils.serialiseJSON(mainBuffer))
+      drawStatus()
+      local err, msg = http.post(streamURL,convertHTML(mainBuffer),{["Content-Type"] = "text/html"})
+      --convertHTML(mainBuffer)
+      if err then error(msg) end
     end
-    sleep(1/targetFPS - (endTime - startTime))
-  end
+end
+
+local function displayLoop()
+    while true do
+        pollDisplay()
+        sleep(0.1)
+    end
+end
+
+local function buttons()
+    local _, key, is_held = os.pullEvent("key")
+    pollDisplay()
+    buttons() -- this **will** break someday
 end
 
 if benchmarkMode then
     pollDisplay()
 end
 
-parallel.waitForAny(runProgram,pollDisplay)
+parallel.waitForAny(runProgram,displayLoop, buttons)
--- server/index.js
+++ server/index.js
@@ -1,22 +1,59 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const Fastify = require('fastify')
+const app = Fastify({
+    logger: true
+})
+const path = require('path')
+
+
 const port = 3000
 
 let buffer = ""
-app.use(bodyParser.raw({ inflate: true, limit: '100kb', type: 'text/xml' })); // stackoverflow my beloved
+
+app.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/', // optional: default '/'
+})
+
+
+app.addContentTypeParser(
+    "text/html",
+    { parseAs: "string" },
+    function (req, body, done) {
+      try {
+        var newBody = {
+          raw: body,
+          //parsed: JSON.parse(body),
+        };
+        done(null, newBody);
+      } catch (error) {
+        error.statusCode = 400;
+        done(error, undefined);
+      }
+    }
+  );
 
 app.get('/', (req, res) => {
+  res.type("text/html")
   res.send(buffer)
+})
+
+app.get("/stream.css",(req,res) => {
+  res.sendFile("stream.css")
+})
+
+app.get("/minecraftia.ttf",(req,res) => {
+  res.sendFile("minecraftia.ttf")
 })
 
 app.post('/', (req, res) => {
     console.log("got post")
-    console.log(req.body)
+    console.log(req.body.raw)
     if(req.body) {
-        buffer = req.body
+        buffer = req.body.raw
     }
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
